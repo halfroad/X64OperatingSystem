a0fc5cd22b7339956c2865c2ff73d81ec6b2cd90
Li, Hui <pub@halfroad.com>
2022-07-28 00:17:38 +0800

    Use the 4-9 for the main.c

--- Chapters/04/Kernel/main.c
+++ Chapters/04/Kernel/main.c
@@ -1,5 +1,47 @@
 void StartKernel(void)
 {
+	int *address = (int *) 0xffff800000a00000;
+
+	for (int i = 0; i < 1440 * 20; i ++)
+	{
+		*((char *) address + 0) = (char) 0x00;
+		*((char *) address + 1) = (char) 0x00;
+		*((char *) address + 2) = (char) 0xff;
+		*((char *) address + 3) = (char) 0x00;
+
+		address += 1;
+	}
+
+	for (int i = 0; i < 1440 * 20; i ++)
+	{
+		*((char *) address + 0) = (char) 0x00;
+		*((char *) address + 1) = (char) 0xff;
+		*((char *) address + 2) = (char) 0x00;
+		*((char *) address + 3) = (char) 0x00;
+
+		address += 1;
+	}
+
+	for (int i = 0; i < 1440 * 20; i ++)
+	{
+		*((char *) address + 0) = (char) 0xff;
+		*((char *) address + 1) = (char) 0x00;
+		*((char *) address + 2) = (char) 0x00;
+		*((char *) address + 3) = (char) 0x00;
+
+		address += 1;
+	}
+
+	for (int i = 0; i < 1440 * 20; i ++)
+	{
+		*((char *) address + 0) = (char) 0xff;
+		*((char *) address + 1) = (char) 0xff;
+		*((char *) address + 2) = (char) 0xff;
+		*((char *) address + 3) = (char) 0x00;
+
+		address += 1;
+	}
+
 	while(1)
 		;
 }
