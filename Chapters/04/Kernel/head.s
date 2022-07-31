f1ce0b3cec25d610dd5add7d54bb77015c44d7f2
LI, Hui <pub@halfroad.com>
2022-07-31 17:44:01 +0800

    Completed the DisplayOnScreen

--- Chapters/04/Kernel/head.s
+++ Chapters/04/Kernel/head.s
@@ -1,9 +1,6 @@
-# 0 "head.S"
-# 0 "<built-in>"
-# 0 "<command-line>"
-# 1 "/usr/include/stdc-predef.h" 1 3 4
-# 0 "<command-line>" 2
 # 1 "head.S"
+# 1 "<built-in>" 1
+# 1 "head.S" 2
 
 
                                     .SECTION .text
--- Chapters/04/Kernel/main.c
+++ Chapters/04/Kernel/main.c
@@ -1,47 +1,24 @@
+#include "lib.h"
+#include "DisplayOnScreen.h"
+
 void StartKernel(void)
 {
-	int *address = (int *) 0xffff800000a00000;
+    int address = (int *) 0xffff800000a00000;
+    
+    int i;
+    
+    Position.HorizontalResolution = 1440;
+    Position.VerticalResolution = 900;
+    
+    Position.HorizontalPosition = 0;
+    Position.VerticalPosition = 0;
+    
+    Position.HorizontalCharacterSize = 8;
+    Position.VerticalCharacterSize = 8;
+    
+    Position.FrameBufferAddress = (int *) 0xffff800000a00000;
+    Position.FrameBufferLength = Position.HorizontalResolution * Position.VerticalResolution * 4;
+    
+    PrintColor(YELLOW, BLACK, "Hello\t\t World!\n")
 
-	for (int i = 0; i < 1440 * 20; i ++)
-	{
-		*((char *) address + 0) = (char) 0x00;
-		*((char *) address + 1) = (char) 0x00;
-		*((char *) address + 2) = (char) 0xff;
-		*((char *) address + 3) = (char) 0x00;
-
-		address += 1;
-	}
-
-	for (int i = 0; i < 1440 * 20; i ++)
-	{
-		*((char *) address + 0) = (char) 0x00;
-		*((char *) address + 1) = (char) 0xff;
-		*((char *) address + 2) = (char) 0x00;
-		*((char *) address + 3) = (char) 0x00;
-
-		address += 1;
-	}
-
-	for (int i = 0; i < 1440 * 20; i ++)
-	{
-		*((char *) address + 0) = (char) 0xff;
-		*((char *) address + 1) = (char) 0x00;
-		*((char *) address + 2) = (char) 0x00;
-		*((char *) address + 3) = (char) 0x00;
-
-		address += 1;
-	}
-
-	for (int i = 0; i < 1440 * 20; i ++)
-	{
-		*((char *) address + 0) = (char) 0xff;
-		*((char *) address + 1) = (char) 0xff;
-		*((char *) address + 2) = (char) 0xff;
-		*((char *) address + 3) = (char) 0x00;
-
-		address += 1;
-	}
-
-	while(1)
-		;
 }
